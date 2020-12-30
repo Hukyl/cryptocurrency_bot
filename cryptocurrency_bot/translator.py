ad170bce3f5f98d250740de827aaf1526d9c32bc
Andrey Shalaev <65027666+Hukyl@users.noreply.github.com>
2020-12-30 23:17:43 +0200

    Delete translator.py

--- cryptocurrency_bot/translator.py
+++
@@ -1,8 +0,0 @@
-from google_trans_new import google_translator 
-
-translator = google_translator()
-
-
-def translate(message:str, destination:str='ru'):
-    new_message = translator.translate(message, lang_tgt=destination)
-    return new_message.strip()  # .strip() because sometimes it returns useless spaces
