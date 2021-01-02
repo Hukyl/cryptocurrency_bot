792f44da054e8ff235d2956a5f90eac4d51c38a4
Andrey Shalaev <65027666+Hukyl@users.noreply.github.com>
2021-01-03 00:24:51 +0200

    Add files via upload

---
+++ cryptocurrency_bot/utils/_datetime.py
@@ -0,0 +1,35 @@
+import datetime
+
+
+def get_current_datetime():
+    n = datetime.datetime.utcnow()
+    n = n.replace(
+        day=n.day + ((n.hour + 3) // 24),
+        hour=(n.hour + 3) % 24,
+        tzinfo=datetime.timezone(datetime.timedelta(0, 3*60*60))
+    )
+    return n
+
+
+def check_datetime_in_future(up_to_date:datetime.datetime):
+    return (get_current_datetime() - up_to_date).total_seconds() <= 0
+
+
+def convert_from_country_format(datetime_str:str, country:str):
+    if country == 'ru':
+        check_str = '%d.%m.%Y %H:%M'
+    elif country == 'en':
+        check_str = '%m-%d-%Y %I:%M %p'
+    else:
+        check_str = '%Y-%m-%d %H:%M'
+    return datetime.datetime.strptime(datetime_str, check_str)
+
+
+def convert_to_country_format(datetime_:datetime.datetime, country:str):
+    if country == 'ru':
+        format_str = '%d.%m.%Y %H:%M'
+    elif country == 'en':
+        format_str = '%m-%d-%Y %I:%M %p'
+    else:
+        format_str = '%Y-%m-%d %H:%M'
+    return datetime_.strftime(format_str)
