bed7e0add464a8cc820d25da0a4960e3664f3fe4
Andrey Shalaev <65027666+Hukyl@users.noreply.github.com>
2021-01-02 20:28:58 +0200

    Add files via upload

---
+++ cryptocurrency_bot/main.py
@@ -0,0 +1,40 @@
+import threading
+import sys
+from time import sleep
+import datetime
+
+import main_bot
+import techsupport_bot
+from utils._datetime import get_current_datetime
+
+
+
+def infinite_loop(func, *args, **kwargs):
+    while True:
+        try:
+            func(*args, **kwargs)
+        except Exception as e:
+            print(repr(e))
+        else:
+            break
+
+
+
+if __name__ == '__main__':
+    targets = [
+        main_bot.check_alarm_times,
+        main_bot.update_rates,
+        main_bot.check_premium_ended,
+        main_bot.verify_predictions,
+        main_bot.bot.infinity_polling,
+        techsupport_bot.bot.infinity_polling
+    ]
+    for target in targets:
+        threading.Thread(target=target, daemon=True).start()
+    print(f"[INFO] Bot started at {str(get_current_datetime().time().strftime('%H:%M:%S'))}")
+    while True:
+        try:
+            sleep(100000)
+        except KeyboardInterrupt:
+            print(f"[INFO] Bot stopped at {str(get_current_datetime().time().strftime('%H:%M:%S'))}")
+            sys.exit(0)
